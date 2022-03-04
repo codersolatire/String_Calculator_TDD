ca40f2a35386c2dff0e16a8618e0dda09be16aa4
Parth Singh <parthsingh.2212@gmail.com>
2022-03-04 14:08:11 +0530

    negatives not allowed

--- index.py
+++ index.py
@@ -2,6 +2,9 @@ import unittest
 import re
 
 def Add(numbers):
+    if len(numbers.split("-")) > 1:
+        return "negatives not allowed"
+        
     delimeters = numbers[numbers.find("//")+len("//"):numbers.find("\n")]
 
     if numbers.find("//") == 0:
@@ -43,6 +46,9 @@ class index(unittest.TestCase):
 
     def test_index_delimeters_and_nums(self):
         self.assertEqual(Add("//;\n1;2"), "3")
+
+    def test_index_with_negative(self):
+        self.assertEqual(Add("40\n10,20,-30,50"), "negatives not allowed")
 
 if __name__ == '__main__':
     unittest.main()
